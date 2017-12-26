3fe9792164bb69f92af0be85aee4744cab9dd760
hefeng <hefeng@iboxpay.com>
2017-12-26 11:09:30 +0800

    [homeIndex] update

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,45 +1,56 @@
 <template>
   <div class="page-home">
     <ul class="home-quick">
-      <li class="money">fdd</li>
-      <li class="pay">fdd</li>
+      <li class="money">
+        <router-link to="/bank/add">
+          <img src="~assets/less/icon/money.png">
+        </router-link>
+        <p>收款</p>
+      </li>
+      <li class="pay">
+        <router-link to="/bank/add">
+          <img src="~assets/less/icon/pay.png">
+        </router-link>
+        <p>还款</p>
+      </li>
     </ul>
 
     <!--九宫格-->
-    <grid class="home-grids">
+    <grid class="home-grids" :rows="2" :cols="3">
       <grid-item link="/bank">
-        <img slot="icon" src="~assets/images/icon-bank.png">
-        <span slot="label">银行卡</span>
+        <img slot="icon" src="~assets/less/icon/wodezhanghu.png">
+        <span slot="label">我的账户</span>
       </grid-item>
       <grid-item link="/bill">
-        <img slot="icon" src="~assets/images/icon-bill.png">
-        <span slot="label">交易记录</span>
+        <img slot="icon" src="~assets/less/icon/wodefenrun.png">
+        <span slot="label">我的分润</span>
       </grid-item>
       <grid-item link="/plan">
-        <img slot="icon" src="~assets/images/icon-plan.png">
-        <span slot="label">查看计划</span>
+        <img slot="icon" src="~assets/less/icon/zhangdanchaxun.png">
+        <span slot="label">账单查询</span>
       </grid-item>
       <grid-item link="/bank">
-        <img slot="icon" src="~assets/images/icon-bank.png">
-        <span slot="label">银行卡</span>
+        <img slot="icon" src="~assets/less/icon/wodeshanghu.png">
+        <span slot="label">我的商户</span>
       </grid-item>
       <grid-item link="/bill">
-        <img slot="icon" src="~assets/images/icon-bill.png">
-        <span slot="label">交易记录</span>
+        <img slot="icon" src="~assets/less/icon/wodefeilv.png">
+        <span slot="label">我的费率</span>
       </grid-item>
       <grid-item link="/plan">
-        <img slot="icon" src="~assets/images/icon-plan.png">
-        <span slot="label">查看计划</span>
+        <img slot="icon" src="~assets/less/icon/xinyongkabanli.png">
+        <span slot="label">信用卡办理</span>
       </grid-item>
     </grid>
 
     <!--广告-->
     <div class="home-ads">
       <swiper class="home-ads__container"
-        v-if="adsList > 0"
+        :height="ads_height"
+        v-if="ads_list.length > 0"
       >
         <swiper-item class="home-ads__item"
-          v-for="(adItem, adIdx) in adsList"
+          v-for="(adItem, adIdx) in ads_list"
           :key="adIdx"
         >
           <img :src="adItem.imgUrl">
@@ -52,7 +63,6 @@
 <script>
 import { Grid, GridItem, Swiper, SwiperItem } from 'vux'
 import { mapState, mapActions } from 'vuex'
-import Bankcard from '~components/Bankcard'
 
 const CARD_STATUS_MAP = {
   "0": "定制计划",
@@ -67,12 +77,11 @@ export default {
     Grid,
     GridItem,
     Swiper,
-    SwiperItem,
-    Bankcard
+    SwiperItem
   },
   data() {
     return {
-
+      baseFontSize: parseFloat(window.document.documentElement.style.fontSize)
     }
   },
   computed: {
@@ -80,10 +89,14 @@ export default {
         mchtInfo: state => state.global.mchtInfo,
         creditCardList: state => state.global.creditCardList
     }),
-    adsList() {
-      return  [
-        { imgUrl: "~assets/images/ads-item.jpg" }
+    ads_list() {
+      return [
+        { imgUrl: require("~assets/images/bankCard.jpg") },
+        { imgUrl: require("~assets/images/ads-item.jpg") }
       ]
+    },
+    ads_height() {
+      return 120/37.5 * this.baseFontSize + "px"
     }
   },
   created () {
@@ -92,7 +105,7 @@ export default {
       queryRules: 'credit_card_home_page',
       pageSize: 20
     }).then((res=[]) => {
-      this.adsList = res
+      this.ads_list = res
     })*/
   },
   methods: {
@@ -108,17 +121,13 @@ export default {
 @import '~assets/less/base/icon';
 @import '~assets/less/base/common';
 
-@userInfo-font-color-link: #D6AA4E;
-@userInfo-bgColor-start: #232736;
-@userInfo-bgColor-end: #323956;
-@userInfo-bgColor-icon: #D8D8D8;
-
 .page-home {
   background: url("~assets/images/bg.png") scroll no-repeat 0 0;
   background-size: 100% auto;
 }
 .home-quick,
-.home-grids {
+.home-grids,
+.home-ads {
   width: 100%;
   height: auto;
   overflow: hidden;
@@ -126,58 +135,61 @@ export default {
 
 /*收款*/
 .home-quick {
+  height: 160/@unit;
   >li {
     width: 50%;
-    height: 100/@unit;
+    height: inherit;
     float: left;
+    margin: 27/@unit 0;
+    img {
+      display: block;
+      width: 80/@unit;
+      height: 80/@unit;
+      margin: 0 auto;
+    }
+    p {
+      height: 26/@unit;
+      line-height: 26/@unit;
+      text-align: center;
+      color: @font-color-white;
+      font-size: 1.8*@font-size-10;
+    }
   }
   .iBox-fn__clearfix()
 }
+
 /*宫格*/
+.home-grids {
+  width: 92%;
+  margin: 10/@unit auto;
+  padding: 10/@unit 0;
+  border-radius: 5/@unit;
+  background-color: @bgColor-white;
+  &:after {
+    border: 0 none !important;
+  }
+  .weui-grid {
+    padding: 10/@unit;
+    &:before,
+    &:after {
+      border: 0 none;
+    }
+  }
+  .weui-grid__icon {
+    width: 41/@unit;
+    height: 41/@unit;
+  }
+  .weui-grid__label {
+    font-size: @font-size-15;
+  }
+}
+
 /*广告*/
-.home-user-profile {
-  color: @font-color-white;
-  .iBox-fn__linear(@toN:-180deg, @startColor:@userInfo-bgColor-start, @endColor:@userInfo-bgColor-end);
-}
-.home-user-profile__info {
-  padding: 16/@unit;
-  >img {
-    display: block;
-    float: left;
-    .iBox-fn__radiusImg(30)
-  }
-  >span {
-    display: block;
-    height: 30/@unit;
-    line-height: 30/@unit;
-  }
-  .home-user-profile__name {
-    float: left;
-    margin-left: 10/@unit;
-    font-size: @font-size-14;
-  }
-  .home-user-profile__link {
-    float: right;
-    font-size: @font-size-13;
-    padding-right: 18/@unit;
-    &.iBox-icon__dbarrow-right {
-      background-size: 18/@unit auto
-    }
-    >a {
-      color: @userInfo-font-color-link;
-    }
-  }
-}
-.home-user-profile__num {
-  >em {
-    opacity: 1;
-    font-style: normal;
-    font-size: @font-size-14;
-  }
-  >span {
-    opacity: 0.5;
-    font-size: @font-size-12;
-  }
+.home-ads {
+  width: 92%;
+  height: 120/@unit;
+  margin: 13/@unit auto;
+  border-radius: 5/@unit;
 }
 
 </style>

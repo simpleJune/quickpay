4faa3bbbc6c6f53b5d7e4bf203e6fe024926f967
hefeng <cn.richard.he@gmail.com>
2017-12-26 17:48:28 +0800

    修改登录密码

--- src/views/settings/pwd.vue
+++ src/views/settings/pwd.vue
@@ -1,23 +1,29 @@
 <template>
-	<div class="page-settings">
-    <group title="基本信息">
-      <cell title="商户名称" value="中商平价超市"></cell>
-      <cell title="姓名" value="何锋"></cell>
-      <cell title="身份证号码" value="422302************3910"></cell>
-      <cell title="手机号" value="136****5555" :is-link="true"></cell>
+	<div class="page-settings_pwd">
+    <group>
+      <cell title="手机号" value="136****6666"></cell>
+      <v-code ref="VCode" :verifyCode.sync="postData.vcode" @sendCodeMsg="sendCodeMsg()"></v-code>
+      <x-input type="text" title="身份证号码" placeholder="您本人身份证号码"></x-input>
+    </group>
+    <group>
+      <x-input type="number" title="新密码" placeholder="6位数字支付密码"></x-input>
+      <x-input type="number" title="确认新密码" placeholder="再次输入以确认"></x-input>
     </group>
   </div>
 </template>
 
 <script>
-import { Group, Cell } from 'vux'
+import { Group, Cell, XInput } from 'vux'
 import { mapState, mapActions } from 'vuex'
+import VCode from '~components/VCode.vue'
 
 export default {
   name: 'page-settings',
   components: {
     Group,
-    Cell
+    Cell,
+    XInput,
+    VCode
   },
   data() {
     return {
@@ -47,11 +53,10 @@ export default {
 <style lang="less">
 @import "~assets/less/views/public";
 
-.page-settings {
+.page-settings_pwd {
   .weui-cell__ft {
     color: @font-color-black;
     font-size: @font-size-14;
   }
 }
-
 </style>
